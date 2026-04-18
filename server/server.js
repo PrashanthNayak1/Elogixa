93f761afef0c6a895765e1c4eb63d2c9c96bad74
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-04-18 08:24:41 +0530

    feat: initialize Express server with database connection and API routing

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173',' https://elogixa.vercel.app'],
+    origin: ['http://localhost:5173', 'https://elogixa.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
