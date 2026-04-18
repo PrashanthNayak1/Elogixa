ba4acc307930875e2ec676555bedcbe0243a46d4
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-04-18 08:18:30 +0530

    feat: initialize React root with GoogleOAuthProvider wrapper

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,9 +7,7 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={clientId}>
+    <App />
+  </GoogleOAuthProvider>,
 )
